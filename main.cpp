ee0188e3d349d7924b30c28027d422e581a7bce2
Ruslan <rrnanbarr@gmail.com>
2022-04-14 15:46:52 +0300

    add homework12_5_3

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    int arr[15];
+    for (int i = 0; i < 15; i++) {
+        std::cin >> arr[i];
+    }
+
+    std::cout << "До сортировки: ";
+    for (int i = 0; i < 15; i++) {
+        std::cout << arr[i] << "  ";
+    }
+
+    int temp;
+    for (int i = 0; i < 15; i++) {
+        for (int j = 14; j >= i + 1; j--) {
+            if (arr[j] < arr[j-1]) {
+                temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+    }
+
+    std::cout << "\nПосле сортировки: ";
+    for (int i = 0; i < 15; i++) {
+        std::cout << arr[i] << "  ";
+    }
+
+}
